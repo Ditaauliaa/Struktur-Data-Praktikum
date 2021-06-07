28df64ad7820771973a8e082e5ad07b4cc163fa7
Ditaauliaa <72481547+Ditaauliaa@users.noreply.github.com>
2021-06-07 08:44:31 +0700

    Add files via upload

---
+++ Praktikum 10/nomer 1.cpp
@@ -0,0 +1,24 @@
+//Dita Aulia Oktavian//
+//20051397020//
+//MIB2020//
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=0;i<20;i++){
+ cout<<A[i]<<" ";
+}
+cout<<"\nData yang ingin dicari : ";
+cin>>c;
+i=0;
+posisi=0;
+while(i<19 && A[i]!=c){
+ i++;
+}
+if (A[i]!=c){
+ cout<<"Maaf data yang dicari tidak ada";
+}else if(posisi=i+1)
+   cout<<"ditemukan pada posisi ke "<<posisi;
+getch();
+}
